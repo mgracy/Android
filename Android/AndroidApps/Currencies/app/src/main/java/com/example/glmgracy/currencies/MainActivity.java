30328fcafb0261c430252ff74c682281a3716cb0
glmgracy <glmgracy@gmail.com>
2016-04-29 16:57:14 +0800

    Gets references to views defined in layout.

--- Android/AndroidApps/Currencies/app/src/main/java/com/example/glmgracy/currencies/MainActivity.java
+++ Android/AndroidApps/Currencies/app/src/main/java/com/example/glmgracy/currencies/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.glmgracy.currencies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mCalcButton;
+    private TextView mConvertedTextView;
+    private EditText mAmountEditText;
+    private Spinner mForSpinner, mHomSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
+        mAmountEditText = (EditText) findViewById(R.id.edt_amount);
+        mCalcButton = (Button)findViewById(R.id.btn_calc);
+        mForSpinner = (Spinner) findViewById(R.id.spn_for);
+        mHomSpinner = (Spinner) findViewById(R.id.spn_hom);
+
     }
 }
